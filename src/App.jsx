5dc1c054f0efb7b699888fc9007989e38c05abc4
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-12-29 22:34:16 +0530

    created components and pages4

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
 import { useState, useEffect } from "react"
 
 import {fetchDataFromApi}  from "./utils/api";
-
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice";
 
 
 function App() {
+  const dispatch = useDispatch();
+  //so now we have stored and now how to use this stored values or retrive by using useSelector
+  //in state we have all the url and genras from homeslice we can take anything 
+  const {url} = useSelector((state) => state.home);
+
+
   useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = () => {
     fetchDataFromApi('/movie/popular').then((res) => {
-      console.log(res)
+      console.log(res);
+      dispatch(getApiConfiguration(res))
     })
   }
   
   return (
  
       <div>
-        
+        {url?.total_pages}
       </div>
       
-  )
+  );
 }
 
 export default App
