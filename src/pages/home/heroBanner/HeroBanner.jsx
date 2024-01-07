f0eff065a18cf2ad308f75d2d565b064cc2234b9
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2024-01-07 21:55:46 +0530

    updated contentWrapper for center align

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -5,6 +5,10 @@ import {useNavigate} from "react-router-dom";
 import { useSelector } from "react-redux";
 import useFetch from '../../../hooks/useFetch';
 
+import Img from '../../../components/lazyLoadImage/Img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+
+
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
@@ -39,7 +43,7 @@ const HeroBanner = () => {
     <div className="heroBanner">
 
       <div className="backdrop-img">
-        
+        <Img  src={background} />
       </div>
 
       <div className="wrapper">
