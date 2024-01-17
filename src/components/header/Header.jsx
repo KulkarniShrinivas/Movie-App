f57d24f9881b188e6583b34bad80e09c1b84bd30
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2024-01-17 21:12:24 +0530

    updated header for mobile and desktop voew

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
+//mobileitem handburger menuitem
 import { SlMenu } from "react-icons/sl";
+//closeButton
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
 
@@ -8,6 +10,7 @@ import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
+import pi from "../../assets/pi.png"
 
 const Header = () => {
     const [show, setShow] = useState("top");
@@ -18,8 +21,53 @@ const Header = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
+
+    //to handle mobile menu we need to create two method
+
+    const openSearch = () => {
+      setMobileMenu(false)
+      setShowSearch(true)
+
+
+    }
+
+    const openMobileMenu = () => {
+      //whenever mobile menu opens we need to make true for setMobileMenu
+      setMobileMenu(true)
+      setShowSearch(false)
+
+
+    }
+
     return (
-        <div>Header</div>
+      <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+          <ContentWrapper>
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            
+            <ul className="menuItems">
+              <li className="menuItem">Movies</li>
+              <li className="menuItem">TV Shows</li>
+              <li className="menuItem">
+                <HiOutlineSearch />
+              </li>
+            </ul>
+
+            <div className="mobileMenuItems">
+              <HiOutlineSearch />
+              {mobileMenu ? (
+                //if its true we need to show close button
+                  <VscChromeClose onClick={() => setMobileMenu(false)} />
+              ) : (
+                //if its false we need to display handburgermenuitem
+                <SlMenu  onClick={openMobileMenu}/>
+              )}
+              
+              
+            </div>
+          </ContentWrapper>
+        </header>
     );
 };
 
